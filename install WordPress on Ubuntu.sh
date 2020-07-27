e74981c2331d8e1035160c3ec766fd5d6be0dd09
Kishore <kishore328@gmail.com>
2020-07-27 22:08:15 +0530

    Intial Commit

--- install WordPress on Ubuntu.sh
+++ install WordPress on Ubuntu.sh
@@ -1,8 +1,26 @@
 #install WordPress on Ubuntu
 
+echo "
+----------------------
+ Apache 2 installing
+----------------------
+"
+
 sudo apt-get install apache2 apache2-utils -y
 
 
+echo "
+----------------------
+ Apache 2 Enabling
+----------------------
+"
+
 sudo systemctl enable apache2
+
+echo "
+----------------------
+ Restarting Apache 2 
+----------------------
+"
 
 sudo systemctl start apache2
